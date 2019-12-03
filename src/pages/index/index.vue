d85926067396c5d88b63a550aa880ff7409fe432
hukai <hooks7@163.com>
2019-12-03 19:28:28 +0800

    mpvue

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     <van-notice-bar left-icon="https://img.yzcdn.cn/1.png"
+                    text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"/>
+
+    <mybr/>
+    <mybr/>
+    <van-notice-bar mode="closeable"
+                    text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"/>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
